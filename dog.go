f1c80881d1d17f4e4a6199fdd301488377218c18
My Name <myEmail@example.com>
2023-07-08 11:22:42 +0800

    v1.0.6

--- dog.go
+++ dog.go
@@ -5,8 +5,8 @@ import "strings"
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
+
+//func returns "human years" in "dog years" (x7)
 func Years(y int) int {
 	return y * 7
 }
-
-///v1.0.2
